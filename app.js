4a5037ba5d1b0672ea0e3355f3b2ae2e8d5d38d6
JacyAnderson <jacyjamesanderson@gmail.com>
2017-05-01 18:00:29 -0600

    setting up backend files

--- app.js
+++ app.js
@@ -1,29 +1,80 @@
-angular.module('news', [])
+angular.module('news', ['ui.router'])
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainController'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsController'
+		});
+
+      // if route doesn't exist, redirect home
+      $urlRouterProvider.otherwise('home');
+    }])
+
+// Posts Factory
 .factory('posts', [function() {
-	let object = {
-		posts: [{title: 'hello', link:'', upvotes: 0}]
+	var object = {
+		posts: []
 	};
-  return object;
+	return object;
 }])
+
+// Main controller
 .controller('MainController', [
 	'$scope', 
 	'posts',
 	function($scope, posts) {
 		$scope.test = 'Hello world!',
-		$scope.posts = posts.posts
+		$scope.posts = posts.posts;
 
 		$scope.addPost = function() {
 			if ($scope.title === '') { return; }
 			$scope.posts.push({
-				title: $scope.title, 
-  			link : $scope.link,
-  			upvotes: 0
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
-		}
+		};
 
-		$scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+ 		// Adds upvotes on click 
+ 		$scope.incrementUpvotes = function(post) {
+ 			post.upvotes += 1;
+ 		};	
+ 	}])
+
+.controller('PostsController', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id];
+
+    // adds comments from form
+		$scope.addComment = function() {
+			if ($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				link: 'user',
+				upvotes: 0
+			});
+
+			// Reset comment body
+			$scope.body = "";
 		}
-	}]);
+	}
+]);
